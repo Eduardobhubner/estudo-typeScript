41f45715ecc732b2d5abae518bbedcca44a17f40
Eduardo balan Hubner <eduardobhubner@gmail.com>
2021-07-21 17:47:17 -0300

    primeira exportacao

---
+++ app/app.ts
@@ -0,0 +1,9 @@
+import { Negociacao } from "./models/Negociacao.js";
+
+let negociacao = new Negociacao(new Date(), 10, 100);
+
+console.log(negociacao.volume);
+
+
+
+
---
+++ dist/js/app.js
@@ -0,0 +1,3 @@
+import { Negociacao } from "./models/Negociacao.js";
+let negociacao = new Negociacao(new Date(), 10, 100);
+console.log(negociacao.volume);
